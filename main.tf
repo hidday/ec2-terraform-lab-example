3c5c7ff41aa2b7955f321dd66e6f51f2f36c5a28
hidday <hiddaysaban@gmail.com>
2024-07-07 15:05:28 +0300

    auto assign ip

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true  # Enable automatic public IP assignment
 }
 
 resource "aws_internet_gateway" "main" {
