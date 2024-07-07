4c991a89a6c5d422466fe4b40a1986c8af54a005
hidday <hiddaysaban@gmail.com>
2024-07-07 16:35:28 +0300

    auto assign ip

--- main.tf
+++ main.tf
@@ -12,6 +12,8 @@ resource "random_password" "password" {
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "main" {
